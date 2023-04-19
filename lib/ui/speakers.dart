c76f9b367367fdb35d02fa533f7cfd065094a015
NdegwaJulius <juliusndegwa2@gmail.com>
2023-04-19 21:25:04 +0300

    Initial commit

---
+++ lib/ui/speakers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Speakers extends StatefulWidget {
+  const Speakers({Key? key}) : super(key: key);
+
+  @override
+  State<Speakers> createState() => _SpeakersState();
+}
+
+class _SpeakersState extends State<Speakers> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
